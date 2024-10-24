236f10761dff52da2b0cb5cbbd276c9ea81be4ad
Dannte <daniel.lopez9804@gmail.com>
2024-10-23 21:40:26 -0500

    Added shader graph and sprites for blocks and breaking visual. Added particle efffect on block braking

--- Space Breaker/Assets/Scripts/Blocks/Block.cs
+++ Space Breaker/Assets/Scripts/Blocks/Block.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private GameObject _particleEffects;
+
     [Header("Values")]
     [SerializeField] private BlockType _blockType;
     [SerializeField] private BlockRuntimeSet _runtimeSet;
 
     private int _resistance;
+    private Material _material;
 
     private void OnEnable() => _runtimeSet.Add(this);
     private void OnDisable() => _runtimeSet.Remove(this);
@@ -14,12 +17,36 @@ public class Block : MonoBehaviour
     private void Start()
     {
         _resistance = _blockType.Resistance;
+        _material = GetComponent<Renderer>().material;
+        _material.SetColor("_BlockColor", _blockType.Colors[0]);
     }
 
     public void HitBlock()
     {
         _resistance--;
         GameManager.Instance.AddScore();
-        if(_resistance <= 0) Destroy(gameObject);
+        if (_resistance > 0)
+        {
+            UpdateMaterial();
+        }
+        else
+        {
+            Instantiate(_particleEffects, transform.position, Quaternion.identity, null);
+            Destroy(gameObject);
+        }
+    }
+
+    private void UpdateMaterial()
+    {
+        Color newColor = _blockType.Colors[_blockType.Resistance - _resistance];
+        _material.SetColor("_BlockColor", newColor);
+        
+        float crackRadius;
+        if (_blockType.Resistance > 1)
+        {
+            crackRadius = (_blockType.Resistance - (float)_resistance) / (_blockType.Resistance - 1);
+            _material.SetFloat("_Radius", crackRadius);
+        }
+
     }
 }
--- Space Breaker/Assets/Scripts/Scriptable Objects/BlockType.cs
+++ Space Breaker/Assets/Scripts/Scriptable Objects/BlockType.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Block Type", menuName = "Scriptable Objects/Block Type")]
 public class BlockType : ScriptableObject
 {
+    [Min(1)]
     public int Resistance;
+    public List<Color> Colors = new();
 }
